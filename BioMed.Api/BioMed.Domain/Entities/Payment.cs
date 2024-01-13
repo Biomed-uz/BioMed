d501e5fe6c50c0c8878b40ee46ba2cedce24a680
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-13 16:45:07 +0500

    Creating Payment Entitiy

---
+++ BioMed.Api/BioMed.Domain/Entities/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace BioMed.Domain.Entities
+{
+    public class Payment : EntityBase
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public int VisitId { get; set; }
+        public Visit? Visit { get; set; }
+    }
+}
