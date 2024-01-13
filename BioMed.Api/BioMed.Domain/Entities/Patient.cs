f42944c72590f75d0c4257e5597d1e24d39bba4f
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-13 16:44:51 +0500

    Creating Patient Entitiy

---
+++ BioMed.Api/BioMed.Domain/Entities/Patient.cs
@@ -0,0 +1,15 @@
+﻿namespace BioMed.Domain.Entities
+{
+    public class Patient : EntityBase
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? PhoneNumber {  get; set; }
+        public string? Email { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string? Gender { get; set; }
+        
+        public virtual ICollection<Visit>? Visits { get; set; }
+    }
+}
