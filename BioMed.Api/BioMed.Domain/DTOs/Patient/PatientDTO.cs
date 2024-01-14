fc3cafee566d7ac59fdc6fc66e3f33798c7de04d
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-14 16:15:12 +0500

    Created PatientDTO

---
+++ BioMed.Api/BioMed.Domain/DTOs/Patient/PatientDTO.cs
@@ -0,0 +1,15 @@
+﻿using BioMed.Domain.DTOs.Visit;
+
+namespace BioMed.Domain.DTOs.Patient
+{
+    public record PatientDTO(
+         int Id,
+         string FirstName,
+         string LastName,
+         string MiddleName,
+         string PhoneNumber,
+         string Email,
+         DateTime RegistrationDate,
+         string Gender,
+         ICollection<VisitDTO> Visits);
+}
