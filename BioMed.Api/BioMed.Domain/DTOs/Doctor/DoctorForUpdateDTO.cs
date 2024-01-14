188e2e0ac75d94ae56084ad4715a9bef8a9e415a
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-14 16:14:16 +0500

    Created DoctorForUpdateDTO

---
+++ BioMed.Api/BioMed.Domain/DTOs/Doctor/DoctorForUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BioMed.Domain.DTOs.Doctor
+{
+    public record DoctorForUpdateDTO(
+        int Id,
+        string FullName,
+        string PhoneNumber,
+        string Email,
+        decimal PricePerVisit,
+        int SpesializationId);
+}
