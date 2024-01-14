a7d69725ffc1cdcdb80ac3dbd82032f2860b0388
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-14 16:18:04 +0500

    Created VisitDTO

---
+++ BioMed.Api/BioMed.Domain/DTOs/Visit/VisitDTO.cs
@@ -0,0 +1,15 @@
+﻿using BioMed.Domain.DTOs.Payment;
+using BioMed.Domain.DTOs.Treatment;
+
+namespace BioMed.Domain.DTOs.Visit
+{
+    public record VisitDTO(
+         int Id,
+         DateTime VisitDate,
+         string Prescription,
+         decimal TotalPrice,
+         int PatientId,
+         int DoctorId,
+         ICollection<TreatmentDTO>? Treatments,
+         ICollection<PaymentDTO>? Payments);
+}
