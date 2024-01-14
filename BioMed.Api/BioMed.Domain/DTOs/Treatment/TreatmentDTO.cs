20c14775583af9a28ed93c7015b18785211dca95
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-14 16:17:23 +0500

    Created TreatmentDTO

---
+++ BioMed.Api/BioMed.Domain/DTOs/Treatment/TreatmentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BioMed.Domain.DTOs.Treatment
+{
+    public record TreatmentDTO(
+        int Id,
+        string Prescription,
+        int VisitId,
+        int LabResultId,
+        int DiseaseId);
+}
