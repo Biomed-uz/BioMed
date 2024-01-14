3914189913be706ed0519f661eb299be76ae778f
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-14 16:10:57 +0500

    Created DiseaseDTO

---
+++ BioMed.Api/BioMed.Domain/DTOs/Disease/DiseaseDTO.cs
@@ -0,0 +1,10 @@
+﻿using BioMed.Domain.DTOs.Treatment;
+
+namespace BioMed.Domain.DTOs.Disease
+{
+    public record DiseaseDTO(
+        int Id,
+        string Name,
+        int DiseaseCategoryId,
+        ICollection<TreatmentDTO> Treatments);
+}
