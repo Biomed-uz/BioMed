cab2df38cd1c444434aecd4a0db9dae8a0d0f1eb
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-14 16:13:56 +0500

    Created DoctorForCreateDTO

---
+++ BioMed.Api/BioMed.Domain/DTOs/Doctor/DoctorForCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BioMed.Domain.DTOs.Doctor
+{
+    public record DoctorForCreateDTO(
+        string FullName,
+        string PhoneNumber,
+        string Email,
+        decimal PricePerVisit,
+        int SpesializationId);
+}
