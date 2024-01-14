bac191935bfe7423bbce0de9f4c4bb2967ed12b3
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-14 15:32:02 +0500

    Creating ICommonRepository Interface

---
+++ BioMed.Api/BioMed.Domain/Interfaces/Repositories/ICommonRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace BioMed.Domain.Interfaces.Repositories
+{
+    public interface ICommonRepository
+    {
+        public ISpesializationRepository Spesialization { get; }
+        public IDepartmentRepository Department { get; }
+        public IDoctorRepository Doctor { get; }
+        public IPatientRepository Patient { get; }
+        public IVisitRepostory Visit {  get; }
+        public IPaymentRepository Payment { get; }
+        public IAnalysisTypeRepository AnalysisType { get; }
+        public ILaboratoryResultRepository LaboratoryResult { get; }
+        public IDiseaseCategoryRepository DiseaseCategory { get; }
+        public IDiseaseRepository Disease { get; }
+        public ITreatmentRepository Treatment { get; }
+
+        public int SaveChanges();
+    }
+}
