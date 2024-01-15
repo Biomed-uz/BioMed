aa0da4295fabc5b4e786aa9136b6a37f6ed5ac8d
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-15 15:17:09 +0500

    Created DoctorResourceParameters

---
+++ BioMed.Api/BioMed.Domain/ResourceParameters/DoctorResourceParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace BioMed.Domain.ResourceParameters
+{
+    public class DoctorResourceParameters : ResourceParametersBase
+    {
+        public int SpesializationId { get; set; }
+        public decimal? PricePerVisit { get; set; }
+        public decimal? PricePerVisitLessThan { get; set; }
+        public decimal? PricePerVisitDreaterThan { get; set; }
+        public override string OrderBy { get; set; } = "fullname";
+
+        protected override int MaxPageSize { get; set; } = 50;
+    }
+}
