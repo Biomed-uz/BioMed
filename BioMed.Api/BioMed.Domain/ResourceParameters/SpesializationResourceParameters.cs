a758b0200a310c3301f2192d63c9ebad719f7102
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-15 15:18:16 +0500

    Created SpesializationResourceParameters

---
+++ BioMed.Api/BioMed.Domain/ResourceParameters/SpesializationResourceParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace BioMed.Domain.ResourceParameters
+{
+    public class SpesializationResourceParameters : ResourceParametersBase
+    {
+        public int DepartmentId { get; set; }
+        public override string OrderBy { get; set; } = "name";
+
+        protected override int MaxPageSize { get; set; } = 50;
+    }
+}
