b10b179eb7d1008bb13fe418188d10dbf864785b
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-15 15:17:32 +0500

    Created LaboratoryResultResourceParameters

---
+++ BioMed.Api/BioMed.Domain/ResourceParameters/LaboratoryResultResourceParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace BioMed.Domain.ResourceParameters
+{
+    public class LaboratoryResultResourceParameters : ResourceParametersBase
+    {
+        public int AnalysisTypeId { get; set; }
+        public override string OrderBy { get; set; } = "name";
+
+        protected override int MaxPageSize { get; set; } = 50;
+    }
+}
