7fa2c3e411faa96ac91d7f9d7120b94d7ee0d162
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 12:00:10 +0500

    Creating Disease Interface Service

---
+++ BioMed.Api/BioMed.Domain/Interfaces/Services/IDiseaseService.cs
@@ -0,0 +1,15 @@
+﻿using BioMed.Domain.DTOs.Disease;
+using BioMed.Domain.Pagination;
+using BioMed.Domain.ResourceParameters;
+
+namespace BioMed.Domain.Interfaces.Services
+{
+    public interface IDiseaseService
+    {
+        PaginatedList<DiseaseDTO> GetDiseases(DiseaseResourceParameters diseaseResourceParameters);
+        DiseaseDTO? GetDiseaseById(int id);
+        DiseaseDTO CreateDisease(DiseaseForCreateDTO diseaseToCreate);
+        void UpdateDisease(DiseaseForUpdateDTO diseaseToUpdate);
+        void DeleteDisease(int id);
+    }
+}
