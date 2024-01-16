0a06509fa98553ea8bdf0bdf344340e191c5dc3a
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 12:01:24 +0500

    Creating Visit Interface Service

---
+++ BioMed.Api/BioMed.Domain/Interfaces/Services/IVisitService.cs
@@ -0,0 +1,15 @@
+﻿using BioMed.Domain.DTOs.Visit;
+using BioMed.Domain.Pagination;
+using BioMed.Domain.ResourceParameters;
+
+namespace BioMed.Domain.Interfaces.Services
+{
+    public interface IVisitService
+    {
+        PaginatedList<VisitDTO> GetVisits(VisitResourceParameters visitResourceParameters);
+        VisitDTO? GetVisitById(int id);
+        VisitDTO CreateVisit(VisitForCreateDTO visitToCreate);
+        void UpdateVisit(VisitForUpdateDTO visitToUpdate);
+        void DeleteVisit(int id);
+    }
+}
