04fcbbd2711f0d22c7d4f0ca87a1b4774c0acfd3
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-01-16 14:40:13 +0500

    Create  SpesializationEntityConfiguration
    defining a database schema for the Specialization entity

---
+++ BioMed.Api/BioMed.Infrastructure/Persistence/Configuration/SpesializationEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using BioMed.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BioMed.Infrastructure.Persistence.Configuration
+{
+    public class SpesializationEntityConfiguration : IEntityTypeConfiguration<Spesialization>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Spesialization> builder)
+        {
+            builder.ToTable(nameof(Spesialization));
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.HasOne(s => s.Department)
+                .WithMany(d => d.Spesializations)
+                .HasForeignKey(s => s.DepartmentId);
+        }
+    }
+}
