1a2833ca76d271bd70947ef8b8c91e2357dd9740
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 11:11:04 +0500

    Deleting asynchronous method

--- BioMed.Api/BioMed.Domain/Pagination/PaginationExtension.cs
+++ BioMed.Api/BioMed.Domain/Pagination/PaginationExtension.cs
@@ -5,19 +5,6 @@ namespace BioMed.Domain.Pagination
 {
     public static class PaginationExtension
     {
-        public static async Task<PaginatedList<T>> ToPaginatedListAsync<T>(
-            this IQueryable<T> source,
-            int pageSize,
-            int pageNumber) where T : EntityBase
-        {
-            var count = await source.CountAsync();
-            var items = await source.Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize)
-                .ToListAsync();
-
-            return new PaginatedList<T>(items, count, pageNumber, pageSize);
-        }
-
         public static PaginatedList<T> ToPaginatedList<T>(
             this IQueryable<T> source,
             int pageSize,
