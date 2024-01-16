2cec3bd4b6bb18cb9eca8f71599299b605b2e2ca
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 12:00:37 +0500

    Creating Patient Interface Service

---
+++ BioMed.Api/BioMed.Domain/Interfaces/Services/IPatientService.cs
@@ -0,0 +1,15 @@
+﻿using BioMed.Domain.DTOs.Patient;
+using BioMed.Domain.Pagination;
+using BioMed.Domain.ResourceParameters;
+
+namespace BioMed.Domain.Interfaces.Services
+{
+    public interface IPatientService
+    {
+        PaginatedList<PatientDTO> GetPatients(PatientResourceParameters patientResourceParameters);
+        PatientDTO? GetPatientById(int id);
+        PatientDTO CreatePatient(PatientForCreateDTO patientToCreate);
+        void UpdatePatient(PatientForUpdateDTO patientToUpdate);
+        void DeletePatient(int id);
+    }
+}
