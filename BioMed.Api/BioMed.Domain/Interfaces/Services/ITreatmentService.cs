7bee6de287d1f9c9f67bc0f6f9281bd015bcfc71
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 12:01:16 +0500

    Creating Treatment Interface Service

---
+++ BioMed.Api/BioMed.Domain/Interfaces/Services/ITreatmentService.cs
@@ -0,0 +1,16 @@
+﻿using BioMed.Domain.DTOs.Treatment;
+using BioMed.Domain.Pagination;
+using BioMed.Domain.ResourceParameters;
+
+namespace BioMed.Domain.Interfaces.Services
+{
+    public interface ITreatmentService
+    {
+        PaginatedList<TreatmentDTO> GetTreatments(TreatmentResourceParameters treatmentResourceParameters);
+        TreatmentDTO? GetTreatmentById(int id);
+        TreatmentDTO CreateTreatment(TreatmentForCreateDTO treatmentToCreate);
+        void UpdateTreatment(TreatmentForUpdateDTO treatmentToUpdate);
+        void DeleteTreatment(int id);
+    }
+}
+
