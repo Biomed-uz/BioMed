71ff116b56e5b5ad19b0f95d591635ae15537336
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-01-16 14:51:16 +0500

    Create AnalysisTypeEntityConfiguration
    defining a database schema for the AnalysisType entity

---
+++ BioMed.Api/BioMed.Infrastructure/Persistence/Configuration/AnalysisTypeEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using BioMed.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BioMed.Infrastructure.Persistence.Configuration
+{
+    public class AnalysisTypeEntityConfiguration : IEntityTypeConfiguration<AnalysisType>
+    {
+        public void Configure(EntityTypeBuilder<AnalysisType> builder)
+        {
+            builder.ToTable(nameof(AnalysisType));
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.HasMany(l => l.LabResults)
+                .WithOne(a => a.AnalysisType)
+                .HasForeignKey(a => a.AnalysisTypeId);
+        }
+    }
+}
