a710c91cbda4da52e11ccb692320adccab9b9e9e
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 12:15:22 +0500

    Creating BioMedDbContext

---
+++ BioMed.Api/BioMed.Infrastructure/Persistence/BioMedDbContext.cs
@@ -0,0 +1,33 @@
+﻿using BioMed.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace BioMed.Infrastructure.Persistence
+{
+    public class BioMedDbContext : DbContext
+    {
+        public virtual DbSet<Spesialization> Spesializations { get; set; }
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<Disease> Diseases { get; set; }
+        public virtual DbSet<DiseaseCategory> DiseaseCategories { get; set; }
+        public virtual DbSet<Doctor> Doctors { get; set; }
+        public virtual DbSet<LaboratoryResult> LaboratoryResults { get; set; }
+        public virtual DbSet<Patient> Patients { get; set; }
+        public virtual DbSet<Payment> Payments { get; set; }
+        public virtual DbSet<AnalysisType> AnalysisTypes { get; set; }
+        public virtual DbSet<Treatment> Treatments { get; set; }
+        public virtual DbSet<Visit> Visits { get; set; }
+
+        public BioMedDbContext(DbContextOptions<BioMedDbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
