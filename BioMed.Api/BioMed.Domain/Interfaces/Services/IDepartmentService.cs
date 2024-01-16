462484f54d916521b8708a365da44314c0bc36c8
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 11:59:49 +0500

    Creating Department Interface Service

---
+++ BioMed.Api/BioMed.Domain/Interfaces/Services/IDepartmentService.cs
@@ -0,0 +1,15 @@
+﻿using BioMed.Domain.DTOs.Department;
+using BioMed.Domain.Pagination;
+using BioMed.Domain.ResourceParameters;
+
+namespace BioMed.Domain.Interfaces.Services
+{
+    public interface IDepartmentService
+    {
+        PaginatedList<DepartmentDTO> GetDepartments(DepartmentResourceParameters departmentResourceParameters);
+        DepartmentDTO? GetDepartmentById(int id);
+        DepartmentDTO CreateDepartment(DepartmentForCreateDTO departmentToCreate);
+        void UpdateDepartment(DepartmentForUpdateDTO departmentToUpdate);
+        void DeleteDepartment(int id);
+    }
+}
