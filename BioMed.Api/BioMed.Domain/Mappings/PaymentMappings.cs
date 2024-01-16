d2fb795b83061db76b85283f64cffeb7f4940dbc
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 11:24:33 +0500

    Creating Payment Mapping

---
+++ BioMed.Api/BioMed.Domain/Mappings/PaymentMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BioMed.Domain.DTOs.Payment;
+using BioMed.Domain.Entities;
+
+namespace BioMed.Domain.Mappings
+{
+    internal class PaymentMappings : Profile
+    {
+        public PaymentMappings() 
+        {
+            CreateMap<Payment, PaymentDTO>();
+            CreateMap<PaymentDTO, Payment>();
+            CreateMap<PaymentForCreateDTO, Payment>();
+            CreateMap<PaymentForUpdateDTO, Payment>();
+        }
+    }
+}
