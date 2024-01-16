8251c3c09a16a1f248e14ab8ebe1faabf7928fdc
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 11:59:22 +0500

    Creating AnaysisType Interface Service

---
+++ BioMed.Api/BioMed.Domain/Interfaces/Services/IAnalysisTypeService.cs
@@ -0,0 +1,15 @@
+﻿using BioMed.Domain.DTOs.AnalysisType;
+using BioMed.Domain.Pagination;
+using BioMed.Domain.ResourceParameters;
+
+namespace BioMed.Domain.Interfaces.Services
+{
+    public interface IAnalysisTypeService
+    {
+        PaginatedList<AnalysisTypeDTO> GetAnalysisTypes(AnalysisTypeResourceParameters analysisTypeResourceParameters);
+        AnalysisTypeDTO? GetvById(int id);
+        AnalysisTypeDTO CreateAnalysisType(AnalysisTypeForCreateDTO analysisTypeToCreate);
+        void UpdateAnalysisType(AnalysisTypeForUpdateDTO analysisTypeToUpdate);
+        void DeleteAnalysisType(int id);
+    }
+}
