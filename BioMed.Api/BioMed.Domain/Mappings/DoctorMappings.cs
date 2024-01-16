8bf7fba1168e85d2c7bd808ee9bf776b663d80df
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 11:24:03 +0500

    Creating Doctor Mapping

---
+++ BioMed.Api/BioMed.Domain/Mappings/DoctorMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BioMed.Domain.DTOs.Doctor;
+using BioMed.Domain.Entities;
+
+namespace BioMed.Domain.Mappings
+{
+    internal class DoctorMappings : Profile
+    {
+        public DoctorMappings() 
+        {
+            CreateMap<Doctor, DoctorDTO>();
+            CreateMap<DoctorDTO, Doctor>();
+            CreateMap<DoctorForCreateDTO, Doctor>();
+            CreateMap<DoctorForUpdateDTO, Doctor>();
+        }
+    }
+}
