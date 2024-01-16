4c7491aa56342d88608a67bec7da24aeac32f9b7
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 11:24:45 +0500

    Creating Spesialization Mapping

---
+++ BioMed.Api/BioMed.Domain/Mappings/SpesializationMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BioMed.Domain.DTOs.Spesialization;
+using BioMed.Domain.Entities;
+
+namespace BioMed.Domain.Mappings
+{
+    internal class SpesializationMappings : Profile
+    {
+        public SpesializationMappings() 
+        {
+            CreateMap<Spesialization, SpesializationDTO>();
+            CreateMap<SpesializationDTO, Spesialization>();
+            CreateMap<SpesializationForCreateDTO, Spesialization>();
+            CreateMap<SpesializationForUpdateDTO, Spesialization>();
+        }
+    }
+}
