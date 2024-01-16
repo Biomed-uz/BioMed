947398e1dbbe58fdbf1b1661a3a7ef1348fb39b3
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 12:00:17 +0500

    Creating Doctor Interface Service

---
+++ BioMed.Api/BioMed.Domain/Interfaces/Services/IDoctorService.cs
@@ -0,0 +1,15 @@
+﻿using BioMed.Domain.DTOs.Doctor;
+using BioMed.Domain.Pagination;
+using BioMed.Domain.ResourceParameters;
+
+namespace BioMed.Domain.Interfaces.Services
+{
+    public interface IDoctorService
+    {
+        PaginatedList<DoctorDTO> GetDoctors(DoctorResourceParameters doctorResourceParameters);
+        DoctorDTO? GetDoctorById(int id);
+        DoctorDTO CreateDoctor(DoctorForCreateDTO doctorToCreate);
+        void UpdateDoctor(DoctorForUpdateDTO doctorToUpdate);
+        void DeleteDoctor(int id);
+    }
+}
