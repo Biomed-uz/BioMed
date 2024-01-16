8573587a945a58845099982ab648c113df061a1b
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 11:23:17 +0500

    Creating AnalysisType Mapping

---
+++ BioMed.Api/BioMed.Domain/Mappings/AnalysisTypeMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BioMed.Domain.DTOs.TestType;
+using BioMed.Domain.Entities;
+
+namespace BioMed.Domain.Mappings
+{
+    internal class TestTypeMappings : Profile
+    {
+        public TestTypeMappings() 
+        {
+            CreateMap<TestType, TestTypeDTO>();
+            CreateMap<TestTypeDTO, TestType>();
+            CreateMap<TestTypeForCreateDTO, TestType>();
+            CreateMap<TestTypeForUpdateDTO, TestType>();
+        }
+    }
+}
