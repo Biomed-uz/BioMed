3e6f65103d769e925b81960143ed42867fc7dbdc
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-16 11:24:16 +0500

    Creating LaboratoryResult Mapping

---
+++ BioMed.Api/BioMed.Domain/Mappings/LaboratoryResultMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BioMed.Domain.DTOs.LabResult;
+using BioMed.Domain.Entities;
+
+namespace BioMed.Domain.Mappings
+{
+    internal class LabResultMappings : Profile
+    {
+        public LabResultMappings() 
+        {
+            CreateMap<LabResult, LabResultDTO>();
+            CreateMap<LabResultDTO, LabResult>();
+            CreateMap<LabResultForCreateDTO, LabResult>();
+            CreateMap<LabResultForUpdateDTO, LabResult>();
+        }
+    }
+}
