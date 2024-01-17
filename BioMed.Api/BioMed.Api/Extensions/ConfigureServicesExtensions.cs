fd7ac4c739b504737bafd52d30e6d4385eedc2d2
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-17 18:07:01 +0500

    Created ConfigureServicesExtensions

---
+++ BioMed.Api/BioMed.Api/Extensions/ConfigureServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BioMed.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BioMed.Api.Extensions
+{
+    public static class ConfigureServicesExtensions
+    {
+        public static IServiceCollection ConfigureDatabaseContext(this IServiceCollection services)
+        {
+            var builder = WebApplication.CreateBuilder();
+
+            services.AddDbContext<BioMedDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("BioMedConnection")));
+
+            return services;
+        }
+    }
+}
